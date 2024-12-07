9597f598e3c04330c2ff73181a18af97f3d38032
VanwPham <vanphamanh2303@gmail.com>
2024-12-07 13:08:06 +0700

    [BE] Logger Db

--- BE/server.js
+++ BE/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {db, checkConnection} = require('./src/config/db');
+const { db, checkDbConnection } = require('./src/config/db');
 
 require('dotenv').config();
 const productRoutes = require('./src/routes/product');
-
-// dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,17 +14,28 @@ app.get('/', (req, res) => {
     res.send('Hello, Backend!');
 });
 
-(async () => {
-    await checkConnection();
-})
+// Kiểm tra kết nối cơ sở dữ liệu và sau đó khởi động server
+const startServer = async () => {
+    try {
+        // Kiểm tra kết nối cơ sở dữ liệu
+        await checkDbConnection();
+
+        // Sau khi kết nối thành công, lắng nghe yêu cầu
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.error('Failed to connect to the database:', err);
+        process.exit(1); // Dừng ứng dụng nếu không kết nối được cơ sở dữ liệu
+    }
+};
+
+// Gọi hàm khởi động server
+startServer();
 
 // Đóng kết nối khi server dừng
 process.on('SIGINT', async () => {
     await db.destroy();
     console.log('Đã đóng kết nối cơ sở dữ liệu!');
     process.exit(0);
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
 });
--- BE/src/config/db.js
+++ BE/src/config/db.js
@@ -1,15 +1,16 @@
 const knex = require('knex');
 const knexConfig = require('../../knexfile');
-// Sử dụng cấu hình 'development' từ knexfile.js
+
 const db = knex(knexConfig.development);
 
 const checkDbConnection = async () => {
     try {
-        await db.raw('SELECT 1');
+        await db.raw('SELECT 0');
         console.log('Database connection successful');
     } catch (err) {
         console.error('Database connection failed:', err.message);
         process.exit(1);
     }
 };
+
 module.exports = {db, checkDbConnection};
