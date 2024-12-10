c49c433a7d7d1d1d50bee8f811575eeb40fabfec
minhman21032004 <phamminhman19052018@gmail.com>
2024-12-10 09:43:21 +0700

    Update index.js

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -4,6 +4,7 @@ import AboutPage from '../views/AboutView.vue'
 import ProductDetailsPage from '../views/ProductDetailsPage.vue'
 import ContactPage from '../views/ContactPage.vue'
 import ProductCategory from '../views/ProductCategory.vue'
+import SearchRes from '../views/SearchRes.vue'
 import LoginView from '../views/LoginView.vue'
 import SignupView from '../views/RegisterView.vue'
 import AdminView from '@/views/AdminView.vue'
@@ -44,10 +45,17 @@ const routes = [
     component: SignupView,
   },
   {
+<<<<<<< HEAD
     path: '/admin/products',
     name: 'admin',
     component: AdminView,
   }
+=======
+    path: '/search',
+    name: 'SearchRes',
+    component: SearchRes,
+  },
+>>>>>>> 5a4113a18381b3602658b2570f9f344e507183aa
 ]
 
 const router = createRouter({
