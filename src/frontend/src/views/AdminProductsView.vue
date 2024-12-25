442ee67e509d5350aac763506e49a0991285cb5d
Burncake <125027861+Burncake@users.noreply.github.com>
2024-12-26 01:36:30 +0700

    Update AdminProductsView.vue

--- src/frontend/src/views/AdminProductsView.vue
+++ src/frontend/src/views/AdminProductsView.vue
@@ -157,7 +157,7 @@ import AdminSidebar from '@/components/AdminSidebar.vue'
 import { ref, onMounted } from 'vue'
 import { PlusIcon, EditIcon, TrashIcon, HomeIcon, BoxIcon, FolderIcon, ShoppingCartIcon, UsersIcon, BarChartIcon, SettingsIcon } from 'lucide-vue-next'
 
-this.$currentSection = ref('Products')
+const currentSection = ref('Products')
 const products = ref([])
 const categories = ref([])
 const showModal = ref(false)
